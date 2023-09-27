401615f1462b768512ccfa715767668983c7c4b4
Kotvoro <kotvoro@gmail.com>
2023-09-27 05:57:46 +0300

    Main functions implemented

--- PasswordManager/src/main/java/CryptoFactory.java
+++ PasswordManager/src/main/java/CryptoFactory.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class CryptoFactrory {
+public class CryptoFactory {
+    public static CryptoProvider getCrypto(EncryptionType type) {
+        switch (type) {
+            case BASE_64: {
+                return new CryptoBase64();
+            }
+        }
+        return null;
+    }
 }
